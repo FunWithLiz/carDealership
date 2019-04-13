18b6f72d9617fd910fa6d86f02cb16bbef8ae4c9
Elizabeth Fun <elizabethfun@gmail.com>
2019-04-13 16:56:05 -0400

    Install Angular Material and add Car Mock Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Car } from './car.service';
 
-@NgModule({
+@NgModule({ 
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [ Car],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
