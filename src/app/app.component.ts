f922fc72edc047ac190b46e0f15d0506dba006cf
Elizabeth Fun <elizabethfun@gmail.com>
2019-04-13 16:05:17 -0400

    Add select/options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'carDealership';
+
+colors = [
+  {
+    name: "red",
+    value: "red"
+  },
+  {
+    name: "black",
+    value: "black"
+  },
+  {
+    name: "white",
+    value: "white"
+  },
+  {
+    name: "gray",
+    value: "gray"
+  }
+];
+
+options = [
+  {
+    name: 'true',
+    value: 'true'
+  },
+  {
+    name: 'false',
+    value: 'false'
+  }
+]
+
+ constructor(){
+
+ }
+
+ ngOnInit(){
+
+ }
+
 }
+
